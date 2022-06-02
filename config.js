8d524b907d2da3680956c48727fd7dc559166a0c
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-03 00:28:10 +0700

    set default config

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     developerId: process.env.DEVELOPER_ID || '561170896480501790',
     prefix: process.env.BOT_PREFIX || 'a',
     slashRegister: {
-        clientId: process.env.CLIENT_ID || '',
-        guildId: process.env.GUILD_ID || ''
+        clientId: process.env.CLIENT_ID || '711905379751034950',
+        guildId: process.env.GUILD_ID || '794187901000744991'
     },
     token: process.env.BOT_TOKEN || ''
 };
