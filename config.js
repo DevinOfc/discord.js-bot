fe70402c0850469ee339e9d91d3db8bb88c164d2
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-04 00:31:43 +0700

    set default config adelia bot

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     developerId: process.env.DEVELOPER_ID || '561170896480501790',
     prefix: process.env.BOT_PREFIX || 'a',
     slashRegister: {
-        clientId: process.env.CLIENT_ID || '711905379751034950',
+        clientId: process.env.CLIENT_ID || '713791568968155136',
         guildId: process.env.GUILD_ID || '794187901000744991'
     },
     token: process.env.BOT_TOKEN || ''
