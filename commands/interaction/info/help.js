aa2c6dc6e7da0a458322f447d12b305f88c7b94f
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-04 13:26:41 +0700

    fix: help menu

--- commands/interaction/info/help.js
+++ commands/interaction/info/help.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, SelectMenuBuilder } = require('discord.js');
 const categoryEmoji = {
     info: 'ℹ️'
 };
