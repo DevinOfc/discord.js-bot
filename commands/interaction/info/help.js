e5dce52060006351f4602aaa2e10bf201fb2a5f7
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-04 08:43:55 +0700

    fix: help fields of undefined

--- commands/interaction/info/help.js
+++ commands/interaction/info/help.js
@@ -8,12 +8,13 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('Blurple')
             .setAuthor({ name: `${client.user.username} Help`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-            .setDescription('This is my available commands list.');
+            .setDescription('This is my available commands list.')
+            .setFields([]);
         for (const category of client.slashCommands.categories) {
             const commands = client.slashCommands.filter(cmd => cmd.category == category);
             const categoryName = category.charAt(0).toUpperCase() + category.slice(1);
             const commandList = commands.map(command => `\`${command.name}\``).join(', ');
-            embed.fields.push({ name: `${categoryName} commands`, value: commandList });
+            embed.data.fields.push({ name: `${categoryName} commands`, value: commandList });
         };
         interaction.editReply({ embeds: [embed] });
     }
--- commands/message/info/help.js
+++ commands/message/info/help.js
@@ -8,11 +8,12 @@ module.exports = {
             .setColor('Blurple')
             .setAuthor({ name: `${client.user.username} Help`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
             .setDescription('This is my available commands list.')
+            .setFields([]);
         for (const category of client.commands.categories) {
             const commands = client.commands.filter(cmd => cmd.category == category);
             const categoryName = category.charAt(0).toUpperCase() + category.slice(1);
             const commandList = commands.map(command => `\`${command.name}\``).join(', ');
-            embed.fields.push({ name: `${categoryName} commands`, value: commandList });
+            embed.data.fields.push({ name: `${categoryName} commands`, value: commandList });
         };  
         message.reply({ embeds: [embed] });
     }
